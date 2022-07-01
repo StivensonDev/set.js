7dcd33596e5dd11e7ac7254d015a19a36c9af3c3
STIVENSON.DEV <96761629+StivensonSS@users.noreply.github.com>
2022-07-01 12:25:43 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const nombreFamilia = [
+    {papa: "Carmelo",
+     mama: "Olfa",
+     hijo: "Roller"   
+    }
+]
+
+const miSet = new Set()
+miSet.add("roller");
+
+const miSet2 = new Set();
+miSet2.add("JavaScript");
+
+console.log(nombreFamilia);
+console.log(miSet);
+console.log(miSet2);
